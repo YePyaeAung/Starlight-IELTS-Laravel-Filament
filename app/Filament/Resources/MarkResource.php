c2c996ef365fdc234251918cc83775b835c12369
Ye Pyae Aung <yepyaeaung1999@gmail.com>
2022-07-29 13:58:17 +0630

    Mark Resource

---
+++ app/Filament/Resources/MarkResource.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MarkResource\Pages;
+use App\Filament\Resources\MarkResource\RelationManagers;
+use App\Models\Course;
+use App\Models\Mark;
+use App\Models\Student;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MarkResource extends Resource
+{
+    protected static ?string $model = Mark::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-check';
+
+    protected static ?int $navigationSort = 5;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make([
+                    Select::make('course_id')
+                        ->label('Course')
+                        ->required()
+                        ->options(Course::all()->pluck('name', 'id')->toArray())
+                        ->reactive()
+                        ->afterStateUpdated(fn (callable  $set) => $set('student_id', null)),
+                    Select::make('student_id')
+                        ->label('Name')
+                        ->required()
+                        ->options(function (callable $get) {
+                            $course = Course::find($get('course_id'));
+                            if (!$course) {
+                                return Course::all()->pluck('name', 'id');
+                            }
+                            return $course->students->pluck('name', 'id');
+                        })
+                        ->reactive(),
+                    TextInput::make('listening')
+                        ->required()
+                        ->maxLength(3),
+                    TextInput::make('speaking')
+                        ->required()
+                        ->maxLength(3),
+                    TextInput::make('reading')
+                        ->required()
+                        ->maxLength(3),
+                    TextInput::make('writing')
+                        ->required()
+                        ->maxLength(3),
+                    TextInput::make('overall')
+                        ->required()
+                        ->maxLength(3),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->sortable(),
+                TextColumn::make('student.name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('course.name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('listening')
+                    ->sortable(),
+                TextColumn::make('speaking')
+                    ->sortable(),
+                TextColumn::make('reading')
+                    ->sortable(),
+                TextColumn::make('writing')
+                    ->sortable(),
+                TextColumn::make('overall')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMarks::route('/'),
+            'create' => Pages\CreateMark::route('/create'),
+            'edit' => Pages\EditMark::route('/{record}/edit'),
+        ];
+    }    
+}
---
+++ database/migrations/2022_07_29_061623_create_marks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('marks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('course_id')->constrained()->cascadeOnDelete();
+            $table->float('listening');
+            $table->float('speaking');
+            $table->float('reading');
+            $table->float('writing');
+            $table->float('overall');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('marks');
+    }
+};
